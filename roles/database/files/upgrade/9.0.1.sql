8090e46d85764c9ed4970af10021b74549fde706
cd <cd@cactus.de>
2026-02-04 15:41:58 +0100

    db: upgrade script ready

--- roles/database/files/upgrade/9.0.1.sql
+++ roles/database/files/upgrade/9.0.1.sql
@@ -5,6 +5,10 @@ ADD COLUMN IF NOT EXISTS created bigint,
 ADD COLUMN IF NOT EXISTS removed bigint,
 ADD COLUMN IF NOT EXISTS owner_mapping_source_id bigint; -- stm_ for source (ip_based, custom_field, name_field, manual) todo
 
+ALTER TABLE rule_owner
+DROP COLUMN IF EXISTS owner,
+DROP COLUMN IF EXISTS rule_metadatum;
+
 
 -- set not null if not done
 DO $$
@@ -52,12 +56,27 @@ BEGIN
     END IF;
 END $$;
 
+-- owner source table
+CREATE TABLE if not EXISTS stm_owner_mapping_source
+(
+    "owner_mapping_source_type_id" Integer PRIMARY KEY,
+    "owner_mapping_source_type_name" Varchar NOT NULL
+);
+-- add owner source
+INSERT INTO stm_owner_mapping_source (owner_mapping_source_type_id, owner_mapping_source_type_name)
+VALUES
+    (1, 'ip_based'),
+    (2, 'custom_field'),
+    (3, 'name_field'),
+    (4, 'manual')
+ON CONFLICT (owner_mapping_source_type_id) DO NOTHING;
+
 -- just one "active" (rule_id, owner_id) + performance
 CREATE UNIQUE INDEX IF NOT EXISTS idx_rule_owner_removed_is_null_unique ON rule_owner (rule_id, owner_id) WHERE removed IS NULL;
 
 -- create fks
-ALTER TABLE rule_owner DROP CONSTRAINT IF EXISTS rule_owner_owner_foreign_key;
-ALTER TABLE rule_owner ADD CONSTRAINT rule_owner_owner_foreign_key FOREIGN KEY (rule_id) REFERENCES rule(rule_id) ON UPDATE RESTRICT ON DELETE CASCADE;
+ALTER TABLE rule_owner DROP CONSTRAINT IF EXISTS rule_owner_rule_foreign_key;
+ALTER TABLE rule_owner ADD CONSTRAINT rule_owner_rule_foreign_key FOREIGN KEY (rule_id) REFERENCES rule(rule_id) ON UPDATE RESTRICT ON DELETE CASCADE;
 
 ALTER TABLE rule_owner DROP CONSTRAINT IF EXISTS rule_owner_created_import_control_foreign_key;
 ALTER TABLE rule_owner ADD CONSTRAINT rule_owner_created_import_control_foreign_key FOREIGN KEY (created) REFERENCES import_control(control_id) ON UPDATE RESTRICT ON DELETE CASCADE;
@@ -69,28 +88,22 @@ ALTER TABLE rule_owner DROP CONSTRAINT IF EXISTS rule_owner_owner_mapping_source
 ALTER TABLE rule_owner ADD CONSTRAINT rule_owner_owner_mapping_source_id_stm_owner_mapping_source_foreign_key FOREIGN KEY (owner_mapping_source_id) 
 REFERENCES stm_owner_mapping_source(owner_mapping_source_type_id) ON UPDATE RESTRICT ON DELETE CASCADE;
 
--- rule_owner table changes done
-
-
--- owner source table
-CREATE TABLE if not EXISTS stm_owner_mapping_source
-(
-    "owner_mapping_source_type_id" Integer PRIMARY KEY,
-    "owner_mapping_source_type_name" Varchar NOT NULL
-);
-
--- add owner source
-INSERT INTO stm_owner_mapping_source (owner_mapping_source_type_id, owner_mapping_source_type_name)
-VALUES
-    (1, 'ip_based'),
-    (2, 'custom_field'),
-    (3, 'name_field'),
-    (4, 'manual')
-ON CONFLICT (owner_mapping_source_type_id) DO NOTHING;
-
 
 -- import_control 
 -- alter import_control delete unused/exported columns 
+-- owner source table
+CREATE TABLE if not EXISTS stm_import
+(
+    "import_type_id" Integer PRIMARY KEY,
+    "import_type_name" Varchar NOT NULL
+);
+
+INSERT INTO stm_import (import_type_id, import_type_name)
+VALUES
+    (1, 'rule'),
+    (2, 'owner')
+ON CONFLICT (import_type_id) DO NOTHING;
+
 
 DO $$
 BEGIN
@@ -101,12 +114,42 @@ BEGIN
           AND column_name='import_type_id'
     ) THEN
         ALTER TABLE import_control
-        ADD COLUMN import_type_id INTEGER NOT NULL;
+        ADD COLUMN import_type_id INTEGER;
+		
+		UPDATE import_control
+		SET import_type_id = 1
+		WHERE import_type_id IS NULL;
+
+		ALTER TABLE import_control
+		ALTER COLUMN import_type_id SET NOT NULL;
     END IF;
 END
 $$;
 
 
+-- change name of fields
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name='import_control_rule'
+          AND column_name='rule_changes_found'
+    ) THEN
+        ALTER TABLE import_control_rule RENAME COLUMN rule_changes_found TO changes_found;
+    END IF;
+
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_name='import_control_rule'
+          AND column_name='any_changes_found'
+    ) THEN
+        ALTER TABLE import_control_rule RENAME COLUMN any_changes_found TO policy_changes_found;
+    END IF;
+END$$;
+
+-- runs without problems in pgadmin
 DO $$
 DECLARE
     col RECORD;
@@ -131,25 +174,3 @@ BEGIN
     END LOOP;
 END
 $$;
-
--- change name of fields
-DO $$
-BEGIN
-    IF EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name='import_control_rule'
-          AND column_name='rule_changes_found'
-    ) THEN
-        ALTER TABLE import_control_rule RENAME COLUMN rule_changes_found TO changes_found;
-    END IF;
-
-    IF EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name='import_control_rule'
-          AND column_name='any_changes_found'
-    ) THEN
-        ALTER TABLE import_control_rule RENAME COLUMN any_changes_found TO policy_changes_found;
-    END IF;
-END$$;
